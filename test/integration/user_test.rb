48c91d8193bd7090464a947c209f155a79eca18b
Richard Hurt <rhurt@cafepress.com>
2011-02-26 22:49:20 -0500

    Modified to use a proper login path.

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -1,27 +1,27 @@
-# require 'integration_test_helper'
+require 'integration_test_helper'
 
-# class UserTest < ActionDispatch::IntegrationTest
-#   fixtures :all
-#   Capybara.current_driver = :akephalos
+class UserTest < ActionDispatch::IntegrationTest
+  fixtures :all
+  Capybara.current_driver = :akephalos
 
-#   test "valid user can log in" do
-#     visit root_path
-#     fill_in "login",    :with => "quentin"
-#     fill_in "password", :with => "monkey"
-#     page.execute_script "$('#loginform').submit();"
+  test "valid user can log in" do
+    visit login_path
+    fill_in "login",    :with => "quentin"
+    fill_in "password", :with => "monkey"
+    page.execute_script "$('#loginform').submit();"
 
-#     visit users_path
-#     assert page.has_content? "Logged in as quentin"  
-#   end
+    visit users_path
+    assert page.has_content? "Logged in as quentin"  
+  end
 
-#   test "invalid user cannot log in" do
-#     visit root_path
-#     fill_in "login",    :with => "quentin"
-#     fill_in "password", :with => "invalidpassword"
-#     page.execute_script "$('#loginform').submit();"
+  test "invalid user cannot log in" do
+    visit login_path
+    fill_in "login",    :with => "quentin"
+    fill_in "password", :with => "invalidpassword"
+    page.execute_script "$('#loginform').submit();"
 
-#     visit users_path
-#     assert page.has_content? "Start Graylog2"
-#   end
+    visit users_path
+    assert page.has_content? "Start Graylog2"
+  end
 
-# end
+end
