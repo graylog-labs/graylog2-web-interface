a33851c9f594b545da83679b7b6f38df899bb655
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2011-12-22 17:06:15 -0500

    Use the facility as the type

--- app/models/message_gateway.rb
+++ app/models/message_gateway.rb
@@ -36,10 +36,7 @@ class MessageGateway
     config_index.blank? ? INDEX_NAME = DEFAULT_INDEX_NAME : INDEX_NAME = config_index
   end
 
-  TYPE_NAME = "message"
-
   index_name(INDEX_NAME)
-  document_type(TYPE_NAME)
 
   @index = Tire.index(INDEX_NAME)
   @default_query_options = { :sort => "created_at desc" }
@@ -57,7 +54,7 @@ class MessageGateway
   end
 
   def self.retrieve_by_id(id)
-    wrap @index.retrieve(TYPE_NAME, id)
+    wrap @index.retrieve(id)
   end
 
   def self.dynamic_search(what, with_default_query_options = false)
@@ -90,6 +87,7 @@ class MessageGateway
   end
 
   def self.all_by_quickfilter(filters, page = 1, opts = {})
+    document_type(filters[:facility]) unless filters[:facility].blank?
     r = search pagination_options(page).merge(@default_query_options) do
       query do
         boolean do
@@ -184,7 +182,7 @@ class MessageGateway
   end
 
   def self.delete_message(id)
-    result = Tire.index(INDEX_NAME).remove(TYPE_NAME, id)
+    result = Tire.index(INDEX_NAME).remove(id)
     Tire.index(INDEX_NAME).refresh
     return false if result.nil? or result["ok"] != true
 
