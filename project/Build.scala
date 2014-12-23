c59697ebe15bea85ed07b4c9225d44275c6746a7
Jochen Schalanda <jochen@torch.sh>
2014-12-23 14:39:13 +0100

    Set version to 0.92.2

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ import com.typesafe.sbt.less.Import.LessKeys
 
 object ApplicationBuild extends Build {
   val appName         = "graylog2-web-interface"
-  val appVersion      = "0.92.2-SNAPSHOT"
+  val appVersion      = "0.92.2"
   val appDependencies = Seq(
     cache,
     javaCore,
